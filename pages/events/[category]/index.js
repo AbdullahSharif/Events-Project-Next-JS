760e471a62c3383e045b3279463331d8b6e8cc0e
AbdullahSharif <abdullah.29.umar@gmail.com>
2023-01-15 22:23:46 +0500

    initial commit

---
+++ pages/events/[category]/index.js
@@ -0,0 +1,12 @@
+const Page = () => {
+    return (
+        <div>
+            <h1>Events Page</h1>
+            <a href="/events/London/event1">Event 1</a>
+            <a href="">Event 2</a>
+            <a href="">Event 3</a>
+
+        </div>
+    );
+}
+export default Page
---
+++ pages/events/index.js
@@ -0,0 +1,14 @@
+
+
+const Index = () => {
+    return (
+        <div>
+            <h1>Events Page</h1>
+
+            <a href="/events/London">London</a>
+            <a href="/events/barcelona">barcelona</a>
+        </div>
+    )
+}
+
+export default Index
